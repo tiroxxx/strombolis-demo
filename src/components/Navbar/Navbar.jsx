9bf43f65af1ed41bdd316fc5770ae592195aaf8f
Christian Marquez <christianmarquez41@gmail.com>
2022-03-08 21:03:14 -0500

    close hamburger menu when clicking one of the links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -51,19 +51,19 @@ const Navbar = () => {
             onClick={() => setToggleMenu(false)}
           />
           <ul className="app__navbar-smallscreen_links">
-            <li className="p__opensans">
+            <li className="p__opensans" onClick={() => setToggleMenu(false)}>
               <a href="#home">Home</a>
             </li>
-            <li className="p__opensans">
+            <li className="p__opensans" onClick={() => setToggleMenu(false)}>
               <a href="#about">About</a>
             </li>
-            <li className="p__opensans">
+            <li className="p__opensans" onClick={() => setToggleMenu(false)}>
               <a href="#menu">Menu</a>
             </li>
-            <li className="p__opensans">
+            <li className="p__opensans" onClick={() => setToggleMenu(false)}>
               <a href="#awards">Awards</a>
             </li>
-            <li className="p__opensans">
+            <li className="p__opensans" onClick={() => setToggleMenu(false)}>
               <a href="#contact">Contact</a>
             </li>
           </ul>
